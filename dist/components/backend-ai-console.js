2a6bc7478be484e5e0dbc82fc6e6e15e63e71dfe
Jeongkyu Shin <inureyes@gmail.com>
2020-12-30 18:31:25 +0900

    Updated to v20.12.6

--- dist/components/backend-ai-console.js
+++ dist/components/backend-ai-console.js
@@ -1,1 +1,1 @@
-export{ah as default}from"./backend-ai-console-3b21519e.js";
+export{ah as default}from"./backend-ai-console-c6cf5b77.js";
---
+++ dist/components/translate-unsafe-html-5b600d0b.js
@@ -0,0 +1,1 @@
+import{S as s,g as o}from"./backend-ai-console-c6cf5b77.js";import{u as a}from"./unsafe-html-4e64f410.js";const t=(t,c,e)=>s((()=>a(o(t,c,e))));export{t};
